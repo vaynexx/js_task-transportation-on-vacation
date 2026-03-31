2e92d87460e2bcf627f35e0bfb2b2326df4d3812
Vitalii <vitalilkrutoy234@gmail.com>
2026-04-01 00:11:55 +0300

    refactor

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,16 +1,19 @@
+const DAILY_RATE = 40;
+const DISCOUNT_3_DAYS = 20;
+const DISCOUNT_7_DAYS = 50;
+
 function calculateRentalCost(days) {
-  let total = days * 40;
+  let total = days * DAILY_RATE;
 
   if (days >= 7) {
-    total -= 50;
-  } else if (days >= 3) {
-    total -= 20;
+    total = total - DISCOUNT_7_DAYS;
+  }
+
+  if (days >= 3 && days < 7) {
+    total = total - DISCOUNT_3_DAYS;
   }
 
   return total;
 }
 
-
 module.exports = calculateRentalCost;
-// TODO: Implement calculateRentalCost
-
